90fb902cb5e6e3987cf7141da58fbfff9ba0a4cf
Ralin Chimev <ralin.chimev@gmail.com>
2016-05-26 00:31:38 +0300

    Add top navigation bar to website layout

--- test/integration/home_page_test.rb
+++ test/integration/home_page_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class HomePageTest < ActionDispatch::IntegrationTest
-  test "home page layout" do
-    get root_path 
+  test 'home page layout' do
+    get root_path
     assert_response :success
-    assert_select "title", "Ralin Chimev personal website"
+    assert_template 'homes/show'
+    assert_select 'title', 'Ralin Chimev personal website'
+    assert_select 'nav a[href=?]', root_path, count: 2
+    assert_select 'footer'
   end
 end
