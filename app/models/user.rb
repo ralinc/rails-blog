1611fb7685a24b550e9e311255565c3820a2f346
Ralin Chimev <ralin.chimev@gmail.com>
2016-06-16 20:04:45 +0300

    Add validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true,
+                   uniqueness: { case_sensitive: false },
+                   length: { minimum: 3, maximum: 16 }
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
