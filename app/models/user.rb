4438a21e7d2c98cf440efc32bd1677e4684bc5c4
Ralin Chimev <ralin.chimev@gmail.com>
2016-06-25 03:58:06 +0300

    Cover users functionality with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :name, presence: true,
-                   uniqueness: { case_sensitive: false },
-                   length: { minimum: 3, maximum: 16 }
+                   length: { minimum: 3, maximum: 100 }
   validates :email, presence: true,
                     length: { maximum: 255 },
                     uniqueness: { case_sensitive: false },
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test 'login with invalid credentials' do
+  test 'login' do
     get login_path
     assert_template 'sessions/new'
-    post login_path, session: { email: '', password: '' }
+    post login_path, session: { email: 'js@example.com', password: 'password' }
+    assert_redirected_to root_path
+  end
+
+  test 'logout' do
+    delete logout_path
+    assert_redirected_to root_path
+  end
+
+  test 'login with wrong credentials fails' do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: 'wrong@example.com', password: '123' }
     assert_template 'sessions/new'
   end
 end
