1f60ee0616ecfdaf9a0f10482344f781f0b633eb
Ralin Chimev <ralin.chimev@gmail.com>
2016-06-25 04:04:31 +0300

    Add index to users email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
 
-  validates :name, presence: true,
-                   length: { minimum: 3, maximum: 100 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates :email, presence: true,
                     length: { maximum: 255 },
                     uniqueness: { case_sensitive: false },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: {
+                      with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+                    }
 
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616171003) do
+ActiveRecord::Schema.define(version: 20160625005820) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,7 @@ ActiveRecord::Schema.define(version: 20160616171003) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
