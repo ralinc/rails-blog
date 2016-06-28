a9afb55944b6b743ba91dc39a679e6146375f5b3
Ralin Chimev <ralin.chimev@gmail.com>
2016-06-28 08:56:19 +0300

    Create the articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#show'
 
   resource :about, only: [:show]
+  resources :articles, only: [:index]
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
