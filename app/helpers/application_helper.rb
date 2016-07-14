4f8bc96f12cf56e0c2cfefb3761bf25d6c6ac1eb
Ralin Chimev <ralin.chimev@gmail.com>
2016-07-14 10:30:19 +0300

    Use markdown to write blog posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,25 @@
 module ApplicationHelper
+  MARKDOWN_OPTIONS = {
+    filter_html: true,
+    link_attributes: { rel: 'nofollow', target: '_blank' },
+    space_after_headers: true,
+    fenced_code_blocks: true
+  }.freeze
+
+  MARKDOWN_EXTENSIONS = {
+    autolink: true,
+    superscript: true,
+    disable_indented_code_blocks: true
+  }.freeze
+
   def gravatar_url(email, size)
     gravatar = Digest::MD5.hexdigest(email).downcase
     "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}"
+  end
+
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(MARKDOWN_OPTIONS)
+    markdown = Redcarpet::Markdown.new(renderer, MARKDOWN_EXTENSIONS)
+    markdown.render(text).html_safe
   end
 end
