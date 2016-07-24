1cf31b2473a0a1a8261fc52e1978dc67d9499cf9
Ralin Chimev <ralin.chimev@gmail.com>
2016-07-25 00:42:39 +0300

    Yet another attempt to bring structure to CSS

--- app/services/markdown_renderer.rb
+++ app/services/markdown_renderer.rb
@@ -2,7 +2,7 @@ class MarkdownRenderer < Redcarpet::Render::HTML
   def paragraph(text)
     if text =~ /NOTE:/
       text.gsub(/^(?:NOTE:)(.*?)(?:\n(?=\n)|\Z)/m) do
-        %(<p class="note">#{Regexp.last_match[1].strip}</p>)
+        %(<p class="article-note">#{Regexp.last_match[1].strip}</p>)
       end
     else
       "<p>#{text}</p>"
