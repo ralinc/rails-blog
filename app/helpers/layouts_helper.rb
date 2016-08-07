c9f16813f7bdb836d696f7663093f6ab4e7e8a38
Ralin Chimev <ralin.chimev@gmail.com>
2016-08-07 19:46:37 +0300

    Remove unnecessary layout helper

--- app/helpers/layouts_helper.rb
+++
@@ -1,9 +0,0 @@
-
-require 'digest/md5'
-
-module LayoutsHelper
-  def gravatar
-    digest = Digest::MD5.hexdigest('ralin.chimev@gmail.com')
-    image_tag("http://gravatar.com/avatar/#{digest}?s=40", class: 'img-circle', alt: 'Ralin Chimev')
-  end
-end
