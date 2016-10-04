42feb5a1a5aba59be6e9d02d75b5ddae03d8b82f
Ralin Chimev <ralin.chimev@gmail.com>
2016-10-04 20:37:14 +0300

    Use :back instead of referrer helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def page_title(title)
     "#{title} - Ralin Chimev".html_safe
   end
-
-  def referer_path
-    URI(request.referer).path
-  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,9 +5,4 @@ describe ApplicationHelper do
     page_title = helper.page_title 'Home'
     expect(page_title).to eq('Home - Ralin Chimev')
   end
-
-  it 'returns referer path' do
-    allow(helper.request).to receive(:referer).and_return('http://www.ralin.net/about')
-    expect(helper.referer_path).to eq('/about')
-  end
 end
