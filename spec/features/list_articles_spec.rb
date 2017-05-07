912e88e76e5d01af0d44e705daf347c3a2446f92
Ralin Chimev <ralin.chimev@gmail.com>
2017-05-07 11:29:37 +0300

    Fix filtering articles by tag

--- spec/features/list_articles_spec.rb
+++ spec/features/list_articles_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
 feature 'Listing articles', js: false do
+  let(:tag) { create :tag }
+  let!(:published_article) { create :article, tags: [tag] }
   let!(:wip_article) { create :wip_article }
-  let!(:published_article) { create :article }
 
-  scenario 'when user is guest show only published articles' do
-    visit articles_path
+  context 'when user is guest' do
+    scenario 'show only published articles' do
+      visit articles_path
 
-    expect(page).to have_content('NUMBER OF POSTS: 1')
-    expect(page).to have_content(published_article.title)
-    expect(page).not_to have_content(wip_article.title)
+      expect(page).to have_content('NUMBER OF POSTS: 1')
+      expect(page).to have_content(published_article.title)
+      expect(page).not_to have_content(wip_article.title)
+    end
+
+    scenario 'filter published articles by tag' do
+      visit tag_path(tag)
+
+      expect(page).to have_content('NUMBER OF POSTS: 1')
+      expect(page).to have_content(published_article.title)
+      expect(page).not_to have_content(wip_article.title)
+    end
   end
 
   context 'when user is authenticated' do
