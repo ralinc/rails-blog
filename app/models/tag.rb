cd0a1b258b62f1f3a6c69f31b34d8c0880a86201
Ralin Chimev <ralin.chimev@gmail.com>
2017-05-27 13:57:30 +0300

    Do not use instance variables in partials
    Although the application is small and trivial, that does not mean we
    should neglect good practices and write clean code. In this commit I
    improve overall on views:
    
    1. Use locals in partials
    2. Extract new partials where applicable
    3. Improve on naming

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :articles, through: :taggings
+
+  def published_articles
+    @published_articles ||= articles.published.decorate
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 describe Tag do
   it { should have_many(:taggings) }
   it { should have_many(:articles).through(:taggings) }
+
+  it 'returns only published articles' do
+    tag = create :tag
+    create :wip_article, tags: [tag]
+    published_article = create :published_article, tags: [tag]
+
+    published_articles = tag.published_articles
+
+    expect(published_articles.size).to eq(1)
+    expect(published_articles.first.title).to eq(published_article.title)
+  end
 end
